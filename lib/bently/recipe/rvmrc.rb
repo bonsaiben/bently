737accadb625500cc5cd6005170e72a02c938db1
Benjamin Sullivan <ben@gmo-eclab.com>
2013-01-18 00:45:23 +0900

    add recipe for creating .rvmrc

---
+++ lib/bently/recipe/rvmrc.rb
@@ -0,0 +1,18 @@
+module Bently
+
+  class Rvmrc < Recipe
+  
+    step :touch_rvmrc
+
+    def touch_rvmrc
+      ruby_version = `rvm current`.chomp
+      shell(
+        lambda{|gemset| "echo \"rvm use #{ruby_version}@#{gemset}\"" }, 
+        :ask => "Enter a gemset name:", 
+        :description => "Touch .rvmrc:\nrvm use #{ruby_version}@<gemset>"
+      )
+    end
+
+  end
+
+end
