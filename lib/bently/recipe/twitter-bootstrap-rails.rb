d8e41c00c2a56c84dd5201268063b6d54556362a
Benjamin Sullivan <bsullivan2@gmail.com>
2013-01-24 01:19:12 +0900

    don't install gems under assets group

--- lib/bently/recipe/twitter-bootstrap-rails.rb
+++ lib/bently/recipe/twitter-bootstrap-rails.rb
@@ -2,9 +2,11 @@ module Bently
 
   class TwitterBootstrapRails < RailsRecipe
 
-    step :add_gem, 'gem "twitter-bootstrap-rails", "~> 2.2.0", :group => :assets'
+    step :add_gem, 'gem "therubyracer"'
+    step :add_gem, 'gem "less-rails"'
+    step :add_gem, 'gem "twitter-bootstrap-rails", "~> 2.2.0"'
     step :shell, 'bundle install'
-    step :shell, 'rails g bootstrap:install'
+    step :shell, 'rails generate bootstrap:install less'
 
   end
 
