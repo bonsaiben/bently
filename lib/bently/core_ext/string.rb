35c4dc9b2c29971940d66b701f026bd535c46d33
Benjamin Sullivan <bsullivan2@gmail.com>
2013-02-23 00:57:58 +0900

    fix indentation

--- lib/bently/core_ext/string.rb
+++ lib/bently/core_ext/string.rb
@@ -4,35 +4,35 @@
 
 class String
 
-    def camelize
-      string = self
-      string = string.sub(/^[a-z\d]*/) { $&.capitalize }
-      string = string.gsub(/\-/,'_')
-      string.gsub(/(?:_|(\/))([a-z\d]*)/i) { "#{$1}#{$2.capitalize}" }.gsub('/', '::')
-    end
+  def camelize
+    string = self
+    string = string.sub(/^[a-z\d]*/) { $&.capitalize }
+    string = string.gsub(/\-/,'_')
+    string.gsub(/(?:_|(\/))([a-z\d]*)/i) { "#{$1}#{$2.capitalize}" }.gsub('/', '::')
+  end
 
-    if Module.method(:const_get).arity == 1
-      def constantize
-        names = self.split('::')
-        names.shift if names.empty? || names.first.empty?
+  if Module.method(:const_get).arity == 1
+    def constantize
+      names = self.split('::')
+      names.shift if names.empty? || names.first.empty?
 
-        constant = Object
-        names.each do |name|
-          constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
-        end
-        constant
+      constant = Object
+      names.each do |name|
+        constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
       end
-    else
-      def constantize
-        names = self.split('::')
-        names.shift if names.empty? || names.first.empty?
-
-        constant = Object
-        names.each do |name|
-          constant = constant.const_defined?(name, false) ? constant.const_get(name) : constant.const_missing(name)
-        end
-        constant
-      end
+      constant
     end
+  else
+    def constantize
+      names = self.split('::')
+      names.shift if names.empty? || names.first.empty?
+
+      constant = Object
+      names.each do |name|
+        constant = constant.const_defined?(name, false) ? constant.const_get(name) : constant.const_missing(name)
+      end
+      constant
+    end
+  end
 
 end
