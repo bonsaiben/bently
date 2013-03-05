f7de6f547c3c9257f76892c7b80185e8bdd6e477
Benjamin Sullivan <bsullivan2@gmail.com>
2013-03-05 16:22:19 +0900

    add recipe delayed-job-mongoid

---
+++ lib/bently/recipe/delayed-job-mongoid.rb
@@ -0,0 +1,16 @@
+module Bently
+  class DelayedJobMongoid < RailsRecipe
+
+    name 'delayed_job Mongoid'
+    category 'gem'
+    description 'downloads and installs delayed_job_mongoid gem'
+    homepage 'https://github.com/collectiveidea/delayed_job/blob/master/README.md'
+    version '3.0'
+
+    def initialize
+      gem 'delayed_job_mongoid'
+      bundle
+    end
+
+  end
+end
